52b878a47b337ea8bac61f30bb41c358efe8afa2
MissDoki <eem_@live.com>
2023-11-15 13:51:18 +0100

    created first command

--- Main.js
+++ Main.js
@@ -1,11 +1,3 @@
-console.log("anything");
-
-function name(n){
-console.log(n)
-}
-
-name("this is a text")
-
 require("dotenv").config();
 const dotenv = process.env;
 let discord = 0;
@@ -13,10 +5,15 @@ console.log(process.env.DISCORD)
 const { Client, Events, GatewayIntentBits } = require("discord.js")
 var discordclient = new Client({intents:[GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent]});
 discordclient.once(Events.ClientReady, discordstarted)
-function discordstarted(){
+async function discordstarted(){
     console.log("bot activated")
 }
-discordclient.on(Events.MessageCreate, message=>discordmessage)
-function discordmessage(message){
-    console.log(message)
+discordclient.on(Events.MessageCreate, message=>discordmessage(message))
+async function discordmessage(message){
+    console.log(message.content)
+    console.log(message.author.username)
+    if (message.content == "!test"){
+        message.channel.send("Test completed you dumbass")
+    }
 }
+discord = discordclient.login(process.env.DISCORD)
