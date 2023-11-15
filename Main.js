947769c975674a9351812d593f6cb786c357e6cc
YOomDev <ype.oomes@gmail.com>
2023-11-15 13:56:23 +0100

    Added comments and did a bit of cleanup

--- Main.js
+++ Main.js
@@ -1,19 +1,28 @@
+// Load the config file with the login tokens
 require("dotenv").config();
-const dotenv = process.env;
+
+// Reserve memory so we can stop the bot later using commands
 let discord = 0;
-console.log(process.env.DISCORD)
+
+// Load the discord bot backend
 const { Client, Events, GatewayIntentBits } = require("discord.js")
-var discordclient = new Client({intents:[GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent]});
-discordclient.once(Events.ClientReady, discordstarted)
-async function discordstarted(){
+let discordClient = new Client({intents:[GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent]});
+
+// Setup the starting message
+discordClient.once(Events.ClientReady, discordStarted)
+async function discordStarted(){
     console.log("bot activated")
 }
-discordclient.on(Events.MessageCreate, message=>discordmessage(message))
-async function discordmessage(message){
+
+// Set up the message listener to trigger the specified function for when the bot receives a message
+discordClient.on(Events.MessageCreate, message=>discordMessage(message))
+async function discordMessage(message){
     console.log(message.content)
     console.log(message.author.username)
     if (message.content == "!test"){
         message.channel.send("Test completed you dumbass")
     }
 }
-discord = discordclient.login(process.env.DISCORD)
+
+// Log in the discord bot and save it to the memory for later use or stopping the bot
+discord = discordClient.login(process.env.DISCORD)
